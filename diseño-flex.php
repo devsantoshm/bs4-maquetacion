61c9b57df7c67322a209f48fbe17736d7d27a97d
SantosHM <dosantoshm@gmail.com>
2019-08-17 18:05:24 -0500

    diseño-flexbox

---
+++ diseño-flex.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>NETWORK | Soluciones Móbiles para empresas</title>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <style>
+        .contenedor {
+            max-width: 80%;
+            margin: 1rem auto;
+        }
+        
+        header {
+            background: #cff821;
+            padding: 1rem;
+        }
+        
+        .contenido {
+            background: #9ce317;
+        }
+        
+        .lateral-1 {
+            background: #16c1c7;
+        }
+        
+        .lateral-2 {
+            background: #f50fae;
+        }
+        
+        footer {
+            background: #0f0e0e;
+            color: #fff;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <div class="contenedor">
+        <header>
+            <a href="#" class="logo">
+                <img src="images/logo.svg" alt="Logo del sitio">
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="#">Enlace 1</a></li>
+                    <li><a href="#">Enlace 2</a></li>
+                    <li><a href="#">Enlace 3</a></li>
+                    <li><a href="#">Enlace 4</a></li>
+                    <li><a href="#">Enlace 5</a></li>
+
+                </ul>
+            </nav>
+        </header>
+
+        <article class="contenido">
+            <h1>CONTENIDO PRINCIPAL</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio fuga iste cum asperiores at dolores quasi pariatur, reprehenderit cumque harum temporibus obcaecati est eum ratione impedit a nulla vero quia.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio fuga iste cum asperiores at dolores quasi pariatur, reprehenderit cumque harum temporibus obcaecati est eum ratione impedit a nulla vero quia.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio fuga iste cum asperiores at dolores quasi pariatur, reprehenderit cumque harum temporibus obcaecati est eum ratione impedit a nulla vero quia.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio fuga iste cum asperiores at dolores quasi pariatur, reprehenderit cumque harum temporibus obcaecati est eum ratione impedit a nulla vero quia.</p>
+        </article>
+
+        <aside class="lateral-1">
+            <h2>LATERAL 1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus quae officia, iste maxime in recusandae eius voluptates, repellat vero nihil consequuntur, illum doloribus, assumenda laboriosam quod voluptatum ipsum aspernatur.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus quae officia, iste maxime in recusandae eius voluptates, repellat vero nihil consequuntur, illum doloribus, assumenda laboriosam quod voluptatum ipsum aspernatur.</p>
+        </aside>
+
+        <aside class="lateral-2">
+            <h2>LATERAL 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus quae officia, iste maxime in recusandae eius voluptates, repellat vero nihil consequuntur, illum doloribus, assumenda laboriosam quod voluptatum ipsum aspernatur.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus quae officia, iste maxime in recusandae eius voluptates, repellat vero nihil consequuntur, illum doloribus, assumenda laboriosam quod voluptatum ipsum aspernatur.</p>
+        </aside>
+
+        <footer>
+            <p>Todos los derechos reservados</p>
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
