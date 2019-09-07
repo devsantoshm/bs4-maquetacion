1b4d250cdb6bba9c4bb14b437011a8f30cb5ffdf
SantosHM <dosantoshm@gmail.com>
2019-09-07 18:46:54 -0500

    dando estilos al header

--- index.php
+++ index.php
@@ -5,32 +5,42 @@
    <!-- Required meta tags -->
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i&display=swap" rel="stylesheet">
+   <link rel="stylesheet" type="text/css" href="css/all.min.css">
+   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="css/estilos.css">
 </head>
 <body>
    <section class="bienvenidos">
-      <header class="encabezado" role="banner">
-         <a href="index.php">
-            <img src="images/logo.svg" alt="Logo del sitio">
-         </a>
+      <header class="encabezado navbar fixed-top" role="banner">
+         <div class="container">
+            <a href="index.php" class="logo">
+               <img src="images/logo.svg" alt="Logo del sitio">
+            </a>
 
-         <button type="button" class="boton-buscar">Boton buscar</button>
-         <button type="button" class="boton-menu">Boton menu</button>
+            <button type="button" class="boton-buscar">
+               <i class="fas fa-search"></i>
+            </button>
+            <button type="button" class="boton-menu">
+               <i class="fa fa-bars" aria-hidden="true"></i>
+            </button>
 
-         <form action="#" id="bloque-buscar">
-            <div class="contenedor-bloque-buscar">
-               <input type="text" placeholder="Buscar...">
-               <input type="submit" value="Buscar">
-            </div>
-         </form>
+            <form action="#" id="bloque-buscar">
+               <div class="contenedor-bloque-buscar">
+                  <input type="text" placeholder="Buscar...">
+                  <input type="submit" value="Buscar">
+               </div>
+            </form>
 
-         <nav id="menu-principal">
-            <ul>
-               <li class="active"><a href="index.php">Inicio</a></li>
-               <li><a href="nosotros.php">Nosotros</a></li>
-               <li><a href="servicios.php">Servicios</a></li>
-               <li><a href="contacto.php">Contacto</a></li>
-            </ul>
-         </nav>
+            <nav id="menu-principal">
+               <ul>
+                  <li class="active"><a href="index.php">Inicio</a></li>
+                  <li><a href="nosotros.php">Nosotros</a></li>
+                  <li><a href="servicios.php">Servicios</a></li>
+                  <li><a href="contacto.php">Contacto</a></li>
+               </ul>
+            </nav>
+         </div>
       </header>
 
       <div class="texto-encabezado">
@@ -116,5 +126,6 @@
    </footer>
 
    <a href="#ir-arriba">Ir arriba</a>
+   <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
