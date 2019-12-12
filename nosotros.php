31b9aed90cbe2294cb1388e101c7e57ecf8b15f4
SantosHM <dosantoshm@gmail.com>
2019-12-12 18:19:54 -0500

    dando estilos a la pagina nosotros

---
+++ nosotros.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+<head>
+   <title>Nosotros - NETWORK | Soluciones Móbiles para empresas</title>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i&display=swap" rel="stylesheet">
+   <link rel="stylesheet" type="text/css" href="css/all.min.css">
+   <!-- Carga de Galeria de imágenes -->
+   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body class="paginas-internas">
+    <section class="bienvenidos">
+      <header class="encabezado navbar fixed-top" role="banner">
+         <div class="container">
+            <a href="index.php" class="logo">
+               <img src="images/logo.svg" alt="Logo del sitio">
+            </a>
+
+            <button type="button" class="boton-buscar" data-toggle="collapse" data-target="#bloque-buscar" aria-expanded="false">
+               <i class="fa fa-search"></i>
+            </button>
+            <!-- hidden-md-up = d-md-none -->
+            <button type="button" class="boton-menu d-md-none" data-toggle="collapse" data-target="#menu-principal" aria-expanded="false">
+               <i class="fa fa-bars" aria-hidden="true"></i>
+            </button>
+
+            <form action="#" id="bloque-buscar" class="collapse">
+               <div class="contenedor-bloque-buscar">
+                  <input type="text" placeholder="Buscar...">
+                  <input type="submit" value="Buscar">
+               </div>
+            </form>
+
+            <nav id="menu-principal" class="collapse">
+               <ul>
+                  <li class="active"><a href="index.php">Inicio</a></li>
+                  <li><a href="nosotros.php">Nosotros</a></li>
+                  <li><a href="servicios.php">Servicios</a></li>
+                  <li><a href="contacto.php">Contacto</a></li>
+               </ul>
+            </nav>
+         </div>
+      </header>
+
+      <div class="texto-encabezado text-center">
+         <div class="container">
+            <h1 class="display-4">
+            Nosotros</h1>
+            <p>¿Quienes somos? y ¿Que hacemos?.</p>   
+         </div>
+      </div>
+
+    </section>
+    <section class="ruta py-1">
+      <div class="container">
+          <div class="row">
+              <div class="col-12 text-right">
+                  <a href="index.html">Inicio</a> » Nosotros
+              </div>
+          </div>
+      </div>
+    </section>
+
+    <main class="py-1">
+      <div class="container">
+        <div class="row">
+          <article class="col-md-8">
+            <h2>Trabajamos para tu éxito</h2>
+            <p>
+                NETWORK apuesta por la simplicidad, la actualidad y la originalidad. Creemos que la innovación creativa es la única vía para promover y fomentar contenidos de calidad que fortalezcan la confianza de los usuarios en las posibilidades y nuevos usos de los productos digitales.
+            </p>
+            <p>
+                Nuestro objetivo es conseguir que clientes y usuarios se sientan conectados en el nuevo mundo de la información con servicios y contenidos que no dificulten su relación, sino que la simplifiquen y la conviertan en un hecho cotidiano, agradable y satisfactorio
+            </p>
+          </article>
+          <aside class="col-md-4">
+            <img src="images/nosotros.svg" alt="Nosotros">
+          </aside>
+        </div>
+      </div>
+    </main>
+   
+    <footer class="piedepagina py-3" role="contentinfo">
+      <div class="container">
+         <p>2016 © NETWORK Todos los derechos reservados</p>
+         <ul class="redes-sociales">
+            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fab fa-youtube-square"></i></a></li>
+         </ul>
+      </div>
+    </footer>
+
+   <a class="ir-arriba" href="#"><i class="fas fa-arrow-circle-up"></i></a>
+   <!-- <script src="js/bootstrap.min.js"></script> -->
+   <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
