db28e2d2760d7e8e8a52804264b9c39afe166313
SantosHM <dosantoshm@gmail.com>
2019-12-18 19:04:21 -0500

    Maquetando la pagina servicios con estilos modal

--- servicios.php
+++ servicios.php
@@ -1,121 +1,148 @@
 <!doctype html>
 <html lang="en">
 <head>
-   <title>Servicios - NETWORK | Soluciones Móbiles para empresas</title>
-   <!-- Required meta tags -->
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i&display=swap" rel="stylesheet">
-   <link rel="stylesheet" type="text/css" href="css/all.min.css">
-   <!-- Carga de Galeria de imágenes -->
-   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-   <link rel="stylesheet" type="text/css" href="css/estilos.css">
+ <title>Servicios - NETWORK | Soluciones Móbiles para empresas</title>
+ <!-- Required meta tags -->
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i&display=swap" rel="stylesheet">
+ <link rel="stylesheet" type="text/css" href="css/all.min.css">
+ <!-- Carga de Galeria de imágenes -->
+ <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="css/estilos.css">
 </head>
 <body class="paginas-internas">
-    <section class="bienvenidos">
-      <header class="encabezado navbar fixed-top" role="banner">
-         <div class="container">
-            <a href="index.php" class="logo">
-               <img src="images/logo.svg" alt="Logo del sitio">
-            </a>
+  <section class="bienvenidos">
+    <header class="encabezado navbar fixed-top" role="banner">
+     <div class="container">
+      <a href="index.php" class="logo">
+       <img src="images/logo.svg" alt="Logo del sitio">
+     </a>
 
-            <button type="button" class="boton-buscar" data-toggle="collapse" data-target="#bloque-buscar" aria-expanded="false">
-               <i class="fa fa-search"></i>
-            </button>
-            <!-- hidden-md-up = d-md-none -->
-            <button type="button" class="boton-menu d-md-none" data-toggle="collapse" data-target="#menu-principal" aria-expanded="false">
-               <i class="fa fa-bars" aria-hidden="true"></i>
-            </button>
+     <button type="button" class="boton-buscar" data-toggle="collapse" data-target="#bloque-buscar" aria-expanded="false">
+       <i class="fa fa-search"></i>
+     </button>
+     <!-- hidden-md-up = d-md-none -->
+     <button type="button" class="boton-menu d-md-none" data-toggle="collapse" data-target="#menu-principal" aria-expanded="false">
+       <i class="fa fa-bars" aria-hidden="true"></i>
+     </button>
 
-            <form action="#" id="bloque-buscar" class="collapse">
-               <div class="contenedor-bloque-buscar">
-                  <input type="text" placeholder="Buscar...">
-                  <input type="submit" value="Buscar">
-               </div>
-            </form>
-
-            <nav id="menu-principal" class="collapse">
-               <ul>
-                  <li><a href="index.php">Inicio</a></li>
-                  <li><a href="nosotros.php">Nosotros</a></li>
-                  <li class="active"><a href="servicios.php">Servicios</a></li>
-                  <li><a href="contacto.php">Contacto</a></li>
-               </ul>
-            </nav>
-         </div>
-      </header>
-
-      <div class="texto-encabezado text-center">
-         <div class="container">
-            <h1 class="display-4">
-            Servicios</h1>
-            <p>Diseñamos APPS para empresas.</p>   
-         </div>
+     <form action="#" id="bloque-buscar" class="collapse">
+       <div class="contenedor-bloque-buscar">
+        <input type="text" placeholder="Buscar...">
+        <input type="submit" value="Buscar">
       </div>
+    </form>
 
-    </section>
+    <nav id="menu-principal" class="collapse">
+     <ul>
+      <li><a href="index.php">Inicio</a></li>
+      <li><a href="nosotros.php">Nosotros</a></li>
+      <li class="active"><a href="servicios.php">Servicios</a></li>
+      <li><a href="contacto.php">Contacto</a></li>
+    </ul>
+  </nav>
+</div>
+</header>
 
-    <section class="ruta py-3">
-      <div class="container">
-          <div class="row">
-              <div class="col-12 text-right">
-                  <a href="index.php">Inicio</a> » Servicios
-              </div>
-          </div>
+<div class="texto-encabezado text-center">
+ <div class="container">
+  <h1 class="display-4">
+  Servicios</h1>
+  <p>Diseñamos APPS para empresas.</p>   
+</div>
+</div>
+
+</section>
+
+<section class="ruta py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-right">
+        <a href="index.php">Inicio</a> » Servicios
       </div>
-    </section>
+    </div>
+  </div>
+</section>
 
-    <main class="py-3 lista-servicios">
-      <div class="container">
+<main class="py-3 lista-servicios">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <article class="item-servicios">
+          <i class="fa fa-bullseye" aria-hidden="true"></i>
+          <h4>Desarrollo de Aplicaciones Mobiles</h4>
+          <p>
+            Desarrollamos aplicaciones móviles y plataformas web. Nuestra principal cualidad está en el desarrollo de software a medida de tus necesidades.
+          </p>
+          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#servicio1">Más información</a>
+        </article>
+      </div>
+      <div class="col-md-4">
+        <article class="item-servicios">
+          <i class="fa fa-bullhorn" aria-hidden="true"></i>
+          <h4>Consultoría Tecnológica</h4>
+          <p>
+            Ofrecemos consultoría tecnológica especializada en movilidad, necesaria previo al desarrollo de cualquier proyecto.
+          </p>
+          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#servicio2">Más información</a>
+        </article>
+      </div>
+      <div class="col-md-4">
+        <article class="item-servicios">
+          <i class="fa fa-paper-plane" aria-hidden="true"></i>
+          <h4>Marketing y publicidad Movil</h4>
+          <p>
+            Desarrollamos campañas publicitarias para tus APP. Hacemos conocer tu marca en el momento exacto para conseguir un mayor inpacto..
+          </p>
+          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#servicio3">Más información</a>
+        </article>
+      </div>
+    </div>
+  </div>
+</main>
+
+<footer class="piedepagina py-3" role="contentinfo">
+  <div class="container">
+   <p>2016 © NETWORK Todos los derechos reservados</p>
+   <ul class="redes-sociales">
+    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+    <li><a href="#"><i class="fab fa-youtube-square"></i></a></li>
+  </ul>
+</div>
+</footer>
+
+<a class="ir-arriba" href="#"><i class="fas fa-arrow-circle-up"></i></a>
+
+<!-- Modal -->
+<div class="modal fade" id="servicio1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Desarrollo de Aplicaciones Mobiles</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
         <div class="row">
-          <div class="col-md-4">
-            <article class="item-servicios">
-              <i class="fa fa-bullseye" aria-hidden="true"></i>
-              <h4>Desarrollo de Aplicaciones Mobiles</h4>
-              <p>
-                Desarrollamos aplicaciones móviles y plataformas web. Nuestra principal cualidad está en el desarrollo de software a medida de tus necesidades.
-              </p>
-              <a href="#" class="btn btn-primary">Más información</a>
-            </article>
+          <div class="col-md-8">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis non error, reprehenderit iusto illo quo? Molestiae, sed blanditiis officiis qui corporis odit dignissimos numquam, nulla quo commodi, alias in maxime. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis non error, reprehenderit iusto illo quo? Molestiae, sed blanditiis officiis qui corporis odit dignissimos numquam, nulla quo commodi, alias in maxime.
           </div>
-          <div class="col-md-4">
-            <article class="item-servicios">
-              <i class="fa fa-bullhorn" aria-hidden="true"></i>
-              <h4>Consultoría Tecnológica</h4>
-              <p>
-                Ofrecemos consultoría tecnológica especializada en movilidad, necesaria previo al desarrollo de cualquier proyecto.
-              </p>
-              <a href="#" class="btn btn-primary">Más información</a>
-            </article>
-          </div>
-          <div class="col-md-4">
-            <article class="item-servicios">
-              <i class="fa fa-paper-plane" aria-hidden="true"></i>
-              <h4>Marketing y publicidad Movil</h4>
-              <p>
-                Desarrollamos campañas publicitarias para tus APP. Hacemos conocer tu marca en el momento exacto para conseguir un mayor inpacto..
-              </p>
-              <a href="#" class="btn btn-primary">Más información</a>
-            </article>
+          <div class="col-md-4 text-center">
+            <img src="images/servicio-1.svg" alt="" width="200" class="img-fluid">
           </div>
         </div>
       </div>
-    </main>
-   
-    <footer class="piedepagina py-3" role="contentinfo">
-      <div class="container">
-         <p>2016 © NETWORK Todos los derechos reservados</p>
-         <ul class="redes-sociales">
-            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fab fa-youtube-square"></i></a></li>
-         </ul>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
       </div>
-    </footer>
-
-   <a class="ir-arriba" href="#"><i class="fas fa-arrow-circle-up"></i></a>
-   <!-- <script src="js/bootstrap.min.js"></script> -->
-   <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    </div>
+  </div>
+</div>
+<!-- <script src="js/bootstrap.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </body>
 </html>
