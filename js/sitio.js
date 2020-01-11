75641fcc4adb62674146ca6b1c35cc98293b4ba9
SantosHM <dosantoshm@gmail.com>
2020-01-10 19:57:12 -0500

    creando un encabezado animado

--- js/sitio.js
+++ js/sitio.js
@@ -26,3 +26,18 @@ $(function () {
     });
 
 });
+
+/*---------------------------------
+CABECERA ANIMADA
+----------------------------------*/
+$(window).scroll(function () {
+
+    var nav = $('.encabezado');
+    var scroll = $(this).scrollTop();
+
+    if (scroll >= 80) {
+        nav.addClass("fondo-menu");
+    } else {
+        nav.removeClass("fondo-menu");
+    }
+});
